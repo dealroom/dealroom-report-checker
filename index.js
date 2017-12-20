f6ca317a7666c4d769914fe0ae23e2ac4aceba1e
rush <johny64@gmail.com>
2017-12-20 13:56:32 +0300

    added look for id feature

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const es = require('event-stream')
 
 const optionDefinitions = [
     { name: 'type', type: String, defaultOption: 'companies' },
-    { name: 'file', type: String }
+    { name: 'file', type: String },
+    { name: 'id', type: String }
 ]
 
 const options = commandLineArgs(optionDefinitions)
@@ -15,6 +16,9 @@ if (!options.file) {
     process.exit(1)
 }
 
+if (options.id) {
+    options.id = parseInt(options.id)
+}
 
 let stream = fs.createReadStream(options.file, {encoding: 'utf8'})
 let parser = JSONStream.parse('items.*')
@@ -26,10 +30,22 @@ stream
         switch (options.type) {
             case 'companies':
             case 'investors':
-                console.log('['+data.id+'] '+data.name)
+                if (options.id) {
+                    if (options.id === parseInt(data.id)) {
+                        console.log(JSON.stringify(data))
+                    }
+                } else {
+                    console.log('['+data.id+'] '+data.name)
+                }
                 break;
             case 'rounds':
-                console.log('['+data.id+'] '+data.round+' ('+data.company_name+')')
+                if (options.id) {
+                    if (options.id === parseInt(data.id)) {
+                        console.log(JSON.stringify(data))
+                    }
+                } else {
+                    console.log('['+data.id+'] '+data.round+' ('+data.company_name+')')
+                }
                 break;
             default:
                 throw new Error('Wrong type provided')
